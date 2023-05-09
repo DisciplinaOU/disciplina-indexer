fd141c5f5f7d5ae38d0872178e54bf1fb6caa3b4
hbb228 <hbb228@protonmail.com>
2023-05-09 08:01:31 +0100

    update

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,56 +1,27 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { BigInt, Bytes, Address } from "@graphprotocol/graph-ts"
 import {
   Disciplina,
-  NewChain,
   NewHeader
 } from "../generated/Disciplina/Disciplina"
-import { ExampleEntity } from "../generated/schema"
+import { PrivateBlockHeader } from "../generated/schema"
 
-export function handleNewChain(event: NewChain): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+function saveBlockHeader(id: string, sender: Address, prevHash: Bytes, merkleRoot: Bytes, size: BigInt): void {
+  const entity = new PrivateBlockHeader(id)
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+  entity.sender = sender;
+  entity.prevHash = prevHash;
+  entity.merkleRoot = merkleRoot;
+  entity.size = size;
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.sender = event.params.sender
-  entity.genesisHash = event.params.genesisHash
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.addressHashCBOR(...)
-  // - contract.constructPrivateHeader(...)
-  // - contract.merkleRootCur(...)
-  // - contract.prevHashCur(...)
-  // - contract.privateHeaderHash(...)
-  // - contract.sizes(...)
+  entity.save();
 }
 
-export function handleNewHeader(event: NewHeader): void {}
+export function handleNewHeader(event: NewHeader): void {
+  saveBlockHeader(
+    event.transaction.hash.toHex(),
+    event.params.sender,
+    event.params.prevHash,
+    event.params.merkleRoot,
+    event.params.size
+  )
+}
